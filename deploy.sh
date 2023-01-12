2aceb4779316246744f734e4f19fdfc50a845ece
LTL <2492908776@qq.com>
2023-01-12 13:35:55 +0800

    Fix Bug

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,21 @@
  
 # 确保脚本抛出遇到的错误
 set -e
- 
-# 生成静态文件， npm run docs:build
+
+# 生成打包
+yarn build
+# 删除原先的打包
 rm -rf github.io/dist/*
 
-# 将build生成的dist目录拷贝至上一层目录中
+# 用新的打包文件替换旧的打包文件
 cp -rf docs/.vuepress/dist/*  github.io/dist/
 
-# 进入生成的文件夹
+# 进入笔记本上线的库（gihub page）
 cd github.io
 
-# git初始化，每次初始化不影响推送
+# git 初始化，每次初始化不影响推送
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果你想要部署到 https://USERNAME.github.io
 git push 
